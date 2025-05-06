9a66ff2f2c25139b835b209ff4e6b0d8a931cdd5
Rain Chen <87171700+CongJyu@users.noreply.github.com>
2025-05-06 23:37:11 +0800

    更新模型，新增批量处理测试代码

--- main.py
+++ main.py
@@ -3,5 +3,21 @@
 
 # Use Python 3.10, with package manager and project manager `uv`.
 
+import os
+import time
+
+
 if __name__ == "__main__":
     print("[ INFO ] This is the DHA-Quality-Evaluation project.")
+    
+    print("[ INFO ] Using Dark Channel Prior Method for Dehazing.")
+    os.system("uv run dark_channel_prior.py")
+    print("[ INFO ] Using Fast Visibility Restoration Method for Dehazing.")
+    os.system("uv run fast_vis_restoration.py")
+    print("[ INFO ] Using AOD-Net for Dehazing.")
+    os.system("uv run AOD_dehaze.py")
+    print("[ INFO ] Using Histogram Equalization Method for Dehazing.")
+    os.system("uv run equalized_hist.py")
+    print("[ INFO ] Use FR-IQA for Evaluation Process.")
+    os.system("uv run FR_IQA.py")
+    print("[ INFO ] All Process Done.")
