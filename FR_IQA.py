2bb91110f6e8046acce81ea54dd176d4d950813e
Rain Chen <chenrain2003@outlook.com>
2025-10-29 16:19:06 +0800

    update fr-iqa

--- FR_IQA.py
+++ FR_IQA.py
@@ -38,21 +38,17 @@ def get_mse(original_image_dir, dehazed_image_dir, result_save_dir):
     cols = ["Image File", "MSE"]
 
     for file_name in dehazed_image_list:
-        original_image = cv2.imread(
-            os.path.join(original_image_dir, file_name)
-        )
-        dehazed_image = cv2.imread(
-            os.path.join(dehazed_image_dir, file_name)
-        )
+        original_image = cv2.imread(os.path.join(original_image_dir, file_name))
+        dehazed_image = cv2.imread(os.path.join(dehazed_image_dir, file_name))
 
         # Convert to gray scale image.
         original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
         dehazed_image = cv2.cvtColor(dehazed_image, cv2.COLOR_BGR2GRAY)
         # Calculate MSE
         current_mse = np.round(
-            np.sum(
-                cv2.subtract(original_image, dehazed_image) ** 2
-            ) / float(original_image.shape[0] * original_image.shape[1]), 6
+            np.sum(cv2.subtract(original_image, dehazed_image) ** 2)
+            / float(original_image.shape[0] * original_image.shape[1]),
+            6,
         )
 
         current_result = [file_name, str(current_mse)]
@@ -60,10 +56,7 @@ def get_mse(original_image_dir, dehazed_image_dir, result_save_dir):
 
         result = pandas.DataFrame(columns=cols, data=mse_result)
         print("[ DEBUG ] MSE Result CSV:\n", result)
-        result.to_csv(
-            os.path.join(result_save_dir, "FR_IQA_MSE.csv"),
-            encoding="UTF-8"
-        )
+        result.to_csv(os.path.join(result_save_dir, "FR_IQA_MSE.csv"), encoding="UTF-8")
 
 
 # Evaluate PSNR
@@ -92,16 +85,14 @@ def get_psnr(original_image_dir, dehazed_image_dir, result_save_dir):
         return -1
 
     for file_name in dehazed_image_list:
-        original_image = cv2.imread(
-            os.path.join(original_image_dir, file_name)
-        )
-        dehazed_image = cv2.imread(
-            os.path.join(dehazed_image_dir, file_name)
-        )
+        original_image = cv2.imread(os.path.join(original_image_dir, file_name))
+        dehazed_image = cv2.imread(os.path.join(dehazed_image_dir, file_name))
         current_psnr = np.round(
             skimage.metrics.peak_signal_noise_ratio(
-                original_image, dehazed_image,
-            ), 6
+                original_image,
+                dehazed_image,
+            ),
+            6,
         )
         current_result = [file_name, str(current_psnr)]
         psnr_result.append(current_result)
@@ -109,8 +100,7 @@ def get_psnr(original_image_dir, dehazed_image_dir, result_save_dir):
         result = pandas.DataFrame(columns=cols, data=psnr_result)
         print("[ DEBUG ] PSNR Result CSV:\n", result)
         result.to_csv(
-            os.path.join(result_save_dir, "FR_IQA_PSNR.csv"),
-            encoding="UTF-8"
+            os.path.join(result_save_dir, "FR_IQA_PSNR.csv"), encoding="UTF-8"
         )
 
 
@@ -140,17 +130,17 @@ def get_ssim(original_image_dir, dehazed_image_dir, result_save_dir):
         return -1
 
     for file_name in dehazed_image_list:
-        original_image = cv2.imread(
-            os.path.join(original_image_dir, file_name)
-        )
-        dehazed_image = cv2.imread(
-            os.path.join(dehazed_image_dir, file_name)
-        )
+        original_image = cv2.imread(os.path.join(original_image_dir, file_name))
+        dehazed_image = cv2.imread(os.path.join(dehazed_image_dir, file_name))
         current_ssim = np.round(
             skimage.metrics.structural_similarity(
-                original_image, dehazed_image,
-                win_size=7, data_range=255, channel_axis=2
-            ), 6
+                original_image,
+                dehazed_image,
+                win_size=7,
+                data_range=255,
+                channel_axis=2,
+            ),
+            6,
         )
         current_result = [file_name, str(current_ssim)]
         ssim_result.append(current_result)
@@ -158,8 +148,7 @@ def get_ssim(original_image_dir, dehazed_image_dir, result_save_dir):
         result = pandas.DataFrame(columns=cols, data=ssim_result)
         print("[ DEBUG ] SSIM Result CSV:\n", result)
         result.to_csv(
-            os.path.join(result_save_dir, "FR_IQA_SSIM.csv"),
-            encoding="UTF-8"
+            os.path.join(result_save_dir, "FR_IQA_SSIM.csv"), encoding="UTF-8"
         )
 
 
@@ -168,63 +157,63 @@ if __name__ == "__main__":
     get_mse(
         original_image_dir="./test-data-dcp/GT",
         dehazed_image_dir="./test-data-dcp/dehazed",
-        result_save_dir="./test-data-dcp/evaluate"
+        result_save_dir="./test-data-dcp/evaluate",
     )
     get_psnr(
         original_image_dir="./test-data-dcp/GT",
         dehazed_image_dir="./test-data-dcp/dehazed",
-        result_save_dir="./test-data-dcp/evaluate"
+        result_save_dir="./test-data-dcp/evaluate",
     )
     get_ssim(
         original_image_dir="./test-data-dcp/GT",
         dehazed_image_dir="./test-data-dcp/dehazed",
-        result_save_dir="./test-data-dcp/evaluate"
+        result_save_dir="./test-data-dcp/evaluate",
     )
     # FVR dehaze.
     get_mse(
         original_image_dir="./test-data-fvr/GT",
         dehazed_image_dir="./test-data-fvr/dehazed",
-        result_save_dir="./test-data-fvr/evaluate"
+        result_save_dir="./test-data-fvr/evaluate",
     )
     get_psnr(
         original_image_dir="./test-data-fvr/GT",
         dehazed_image_dir="./test-data-fvr/dehazed",
-        result_save_dir="./test-data-fvr/evaluate"
+        result_save_dir="./test-data-fvr/evaluate",
     )
     get_ssim(
         original_image_dir="./test-data-fvr/GT",
         dehazed_image_dir="./test-data-fvr/dehazed",
-        result_save_dir="./test-data-fvr/evaluate"
+        result_save_dir="./test-data-fvr/evaluate",
     )
     # AOD-Net dehaze
     get_mse(
         original_image_dir="./test-data-aod/GT",
         dehazed_image_dir="./test-data-aod/dehazed",
-        result_save_dir="./test-data-aod/evaluate"
+        result_save_dir="./test-data-aod/evaluate",
     )
     get_psnr(
         original_image_dir="./test-data-aod/GT",
         dehazed_image_dir="./test-data-aod/dehazed",
-        result_save_dir="./test-data-aod/evaluate"
+        result_save_dir="./test-data-aod/evaluate",
     )
     get_ssim(
         original_image_dir="./test-data-aod/GT",
         dehazed_image_dir="./test-data-aod/dehazed",
-        result_save_dir="./test-data-aod/evaluate"
+        result_save_dir="./test-data-aod/evaluate",
     )
     # Histogram Enhancement dehaze.
     get_mse(
         original_image_dir="./test-data-hist/GT",
         dehazed_image_dir="./test-data-hist/dehazed",
-        result_save_dir="./test-data-hist/evaluate"
+        result_save_dir="./test-data-hist/evaluate",
     )
     get_psnr(
         original_image_dir="./test-data-hist/GT",
         dehazed_image_dir="./test-data-hist/dehazed",
-        result_save_dir="./test-data-hist/evaluate"
+        result_save_dir="./test-data-hist/evaluate",
     )
     get_ssim(
         original_image_dir="./test-data-hist/GT",
         dehazed_image_dir="./test-data-hist/dehazed",
-        result_save_dir="./test-data-hist/evaluate"
+        result_save_dir="./test-data-hist/evaluate",
     )
